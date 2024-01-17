63f2f446a028eb67f431433a4aecc82c0a579248
Gazalrafi <grafi7485@gmail.com>
2024-01-18 00:09:26 +0530

    initial commit

---
+++ src/pages/FormSubmit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FormSubmit = () => {
+  return (
+    <div >
+        <h3>Thank you For Your Response!</h3>
+        <h3>Your Form is Submitted</h3>
+    </div>
+  )
+}
+
+export default FormSubmit
